eb178575529b55909a99a71a89135e6f56e86cd1
Manuel-Smith <riddicksmith37@gmail.com>
2023-02-13 19:12:09 +0300

    Added instructions for convert to Romans

--- Palindrome checker/index.js
+++ Palindrome checker/index.js
@@ -1,5 +1,15 @@
-function palindrome(str){
-    return true;
+function palindrome(str) {
+    let regexp = /[a-zA-Z0-9]/g
+
+    let strRgx = str.match(regexp).join("")
+    
+    let reverseArr = [];
+
+    for(let i = 0; i < strRgx.length; i++){
+        reverseArr.unshift(strRgx[i]);
+    }
+
+    return reverseArr.join("").toUpperCase() === strRgx.toUpperCase();
 }
 
-palindrome("eye");
+console.log(palindrome("ex &e"));
