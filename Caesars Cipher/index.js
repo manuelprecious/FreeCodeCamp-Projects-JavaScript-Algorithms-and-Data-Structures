f7e9153c37f83e472704ab451c5b1780e9034978
Manuel-Smith <riddicksmith37@gmail.com>
2023-02-14 20:47:23 +0300

    Fixed bug in Caesars

--- Caesars Cipher/index.js
+++ Caesars Cipher/index.js
@@ -1,5 +1,53 @@
 function rot13(str) {
-    return str;
+  let splitEncryptedStr = str.split("");
+  let decryptedLetters = [];
+  let letter = /[a-zA-Z]/;
+
+  let letters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  for (let i = 0; i < splitEncryptedStr.length; i++) {
+    let greaterThan13 = letters.indexOf(splitEncryptedStr[i]) >= 13;
+    let isAlphabet = letter.test(splitEncryptedStr[i]);
+    let lessthanOrEqual13 = letters.indexOf(splitEncryptedStr[i]) < 13;
+
+    if (greaterThan13 && isAlphabet) {
+      let newIndex = letters.indexOf(splitEncryptedStr[i]) - 13;
+      decryptedLetters.push(letters[newIndex].toUpperCase());
+    } else if (lessthanOrEqual13 && isAlphabet) {
+      let newIndex = letters.indexOf(splitEncryptedStr[i]) + 13;
+      decryptedLetters.push(letters[newIndex].toUpperCase());
+    } else {
+        decryptedLetters.push(splitEncryptedStr[i]);
+    }
   }
-  
-  rot13("SERR PBQR PNZC");
+  return decryptedLetters.join("");
+}
+
+console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
