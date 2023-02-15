22fae5d1117c6ba7dd348dd3c9c6aaf22d50d199
Manuel-Smith <riddicksmith37@gmail.com>
2023-02-15 16:57:40 +0300

    Added solution for telephone number validators

--- Telephone Number Validator/index.js
+++ Telephone Number Validator/index.js
@@ -1,5 +1,19 @@
 function telephoneCheck(str) {
+  let regexNormalNumbers = /^\d{3}\s?\-?\d{3}\-?\s?\d{4}$/;
+  let bracketRegex = /^\(\d{3}\)\s?\d{3}\-\d{4}$/;
+  let startWithOneNormalRegex = /^1\s\d{3}\s?\-?\d{3}\-?\s?\d{4}$/;
+  let startWithOneBracketRegex = /^1\s?\(\d{3}\)\s?\d{3}\-\d{4}$/;
+
+  if (
+    regexNormalNumbers.test(str) ||
+    bracketRegex.test(str) ||
+    startWithOneNormalRegex.test(str) ||
+    startWithOneBracketRegex.test(str)
+  ) {
     return true;
   }
-  
-  telephoneCheck("555-555-5555");
+
+  return false;
+}
+
+console.log(telephoneCheck("555-555-5555"));
